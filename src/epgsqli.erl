bea08447b456ce6ec19bce045a63271bc8d1aaaa
zhouzb <zhouzb@emqx.io>
2021-12-28 15:13:25 +0800

    chore: support async prepared_query2

--- src/epgsqli.erl
+++ src/epgsqli.erl
@@ -18,6 +18,7 @@
          squery/2,
          equery/2, equery/3,
          prepared_query/3,
+         prepared_query2/3, prepared_query2/4, prepared_query2/5,
          parse/2, parse/3, parse/4,
          describe/2, describe/3,
          bind/3, bind/4,
@@ -96,6 +97,15 @@ equery(C, Statement, TypedParameters) ->
 -spec prepared_query(epgsql:connection(), epgsql:statement(), [epgsql:typed_param()]) -> reference().
 prepared_query(C, Statement, TypedParameters) ->
     incremental(C, epgsql_cmd_prepared_query, {Statement, TypedParameters}).
+
+prepared_query2(C, SQL, Parameters) ->
+    prepared_query2(C, SQL, SQL, Parameters, []).
+
+prepared_query2(C, Name, SQL, Parameters) ->
+    prepared_query2(C, Name, SQL, Parameters, []).
+
+prepared_query2(C, Name, SQL, Parameters, Types) ->
+    incremental(C, epgsql_cmd_prepared_query2, {Name, SQL, Parameters, Types}).
 
 -spec parse(epgsql:connection(), epgsql:sql_query()) -> reference().
 parse(C, Sql) ->
